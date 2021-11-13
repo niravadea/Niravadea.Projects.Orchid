402aba8b41e41fc84f863c4b11728b5f6fce0c2b
niravadea <niravadea@gmail.com>
2021-11-13 17:08:24 -0500

    feat: add server-side exception catch

--- Niravadea.Projects.Orchid.Core/Handlers/Interactions/GrantRoleErrorHandler.cs
+++ Niravadea.Projects.Orchid.Core/Handlers/Interactions/GrantRoleErrorHandler.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using DSharpPlus.Exceptions;
+using MediatR;
 using MediatR.Pipeline;
 using Microsoft.Extensions.Logging;
 using Niravadea.Projects.Orchid.Core.Exceptions;
@@ -10,6 +11,7 @@ namespace Niravadea.Projects.Orchid.Core.Handlers.Interactions
 {
     public class GrantRoleErrorHandler
         : IRequestExceptionAction<GrantRoleRequest, RoleNotFoundException>
+        , IRequestExceptionAction<GrantRoleRequest, UnauthorizedException>
     {
         private readonly IMediator _mediator;
         private readonly ILogger<GrantRoleErrorHandler> _logger;
@@ -23,6 +25,7 @@ namespace Niravadea.Projects.Orchid.Core.Handlers.Interactions
             _logger = logger;
         }
 
+        // external server side issue
         public async Task Execute(GrantRoleRequest request, RoleNotFoundException exception, CancellationToken cancellationToken)
         {
             _logger.LogError(exception, "An error occurred while trying to grant an authenticated role.");
@@ -31,5 +34,16 @@ namespace Niravadea.Projects.Orchid.Core.Handlers.Interactions
                 message: "Completed authentication, but the server does not have the necessary role!  Please `/authenticate` again after the server adds the necessary role."
             ));
         }
+
+        // external server side issue
+        public async Task Execute(GrantRoleRequest request, UnauthorizedException exception, CancellationToken cancellationToken)
+        {
+            _logger.LogError(exception, $"An error occurred while trying to grant an authenticated role on interaction '{request.InteractionId}'.");
+            await _mediator.Send(CompleteUnsuccessfulInteractionRequest.NewRequestFromMessage(
+                interactionId: request.InteractionId,
+                message: "Unable to complete the authentication request.  The bot does not have the necessary permissions on this server."
+            ));
+        }
+
     }
 }
