04e1ab2ee6931eb4a036af8fc77d0ad1c3a59ac6
niravadea <niravadea@gmail.com>
2021-11-13 17:02:23 -0500

    feat: add request for clearing tokens

---
+++ Niravadea.Projects.Orchid.Core/Handlers/Tokens/TokenClearRequestHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Niravadea.Projects.Orchid.Core.Requests.Tokens;
+using Niravadea.Projects.Orchid.Core.Services;
+
+namespace Niravadea.Projects.Orchid.Core.Handlers.Tokens
+{
+    public class TokenClearRequestHandler : IRequestHandler<TokenClearRequest>
+    {
+        private readonly ITokenTracker _tokenTracker;
+
+        public TokenClearRequestHandler (ITokenTracker tokenTracker)
+        {
+            _tokenTracker = tokenTracker;
+        }
+
+        public async Task<Unit> Handle(TokenClearRequest request, CancellationToken cancellationToken) {
+            await _tokenTracker.ClearPendingAuthenticationToken(
+                discordId: request.DiscordId
+            );
+            return Unit.Value;
+        }
+    }
+}
