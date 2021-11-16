aa819beaa6602155845e3c6c2b5afd99b2b13011
niravadea <niravadea@gmail.com>
2021-11-15 22:45:10 -0500

    refactor: extract core configuration to class

---
+++ Niravadea.Projects.Orchid.Core/CoreComponentConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+using System.Security.Cryptography;
+
+namespace Niravadea.Projects.Orchid.Core
+{
+    public static class CoreComponentConfiguration
+    {
+        public static void ApplicationConfiguration(IConfigurationBuilder configuration)
+        {
+
+        }
+
+        public static void ServiceConfiguration(HostBuilderContext context, IServiceCollection services)
+        {
+            // add IOptions<T> processing
+            services.AddOptions();
+
+            // add the mediator
+            services
+                .AddMediatR(Assembly.GetEntryAssembly());
+
+            // register services
+            services
+                // register RNG service
+                .AddSingleton<RandomNumberGenerator, RNGCryptoServiceProvider>();
+        }
+
+        public static void LoggingConfiguration(ILoggingBuilder logging)
+        {
+
+        }
+    }
+}
--- Niravadea.Projects.Orchid.Core/Program.cs
+++ Niravadea.Projects.Orchid.Core/Program.cs
@@ -1,16 +1,10 @@
-﻿using MediatR;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Hosting;
 using Niravadea.Projects.Orchid.AuthenticationDatabase;
 using Niravadea.Projects.Orchid.AuthenticationDatabase.LiteDb;
 using Niravadea.Projects.Orchid.Client.External.Discord;
 using Niravadea.Projects.Orchid.Client.Internal.Forum;
 using Niravadea.Projects.Orchid.Shared;
 using Niravadea.Projects.Orchid.TokenTracker;
-using System.Reflection;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace Niravadea.Projects.Orchid.Core
@@ -20,42 +14,17 @@ namespace Niravadea.Projects.Orchid.Core
         static async Task Main(string[] args) =>
             await Host
             .CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration(ApplicationConfiguration)
-            .ConfigureServices(ServiceConfiguration)
+            .ConfigureAppConfiguration(CoreComponentConfiguration.ApplicationConfiguration)
+            .ConfigureServices(CoreComponentConfiguration.ServiceConfiguration)
             .ConfigureServices(SharedComponentConfiguration.ServiceConfiguration)
             .ConfigureServices(InternalForumClientConfiguration.ServiceConfiguration)
             .ConfigureServices(ExternalDiscordClientConfiguration.ServiceConfiguration)
             .ConfigureServices(TokenTrackerConfiguration.ServiceConfiguration)
             .ConfigureServices(AuthenticationDatabaseConfiguration.ServiceConfiguration)
             .ConfigureServices(LiteDbAuthenticationDatabaseConfiguration.ServiceConfiguration)
-            .ConfigureLogging(LoggingConfiguration)
+            .ConfigureLogging(CoreComponentConfiguration.LoggingConfiguration)
             .Build()
             .RunAsync()
             ;
-
-        private static void ApplicationConfiguration(IConfigurationBuilder configuration)
-        {
-
-        }
-
-        private static void ServiceConfiguration(HostBuilderContext context, IServiceCollection services)
-        {
-            // add IOptions<T> processing
-            services.AddOptions();
-
-            // add the mediator
-            services
-                .AddMediatR(Assembly.GetEntryAssembly());
-
-            // register services
-            services
-                // register RNG service
-                .AddSingleton<RandomNumberGenerator, RNGCryptoServiceProvider>();
-        }
-
-        private static void LoggingConfiguration(ILoggingBuilder logging)
-        {
-
-        }
     }
 }
